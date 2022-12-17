3eae37ad8089bd393b51e3d73b1dfda81a40314d
BigRock88 <120140223+BigRock88@users.noreply.github.com>
2022-12-17 11:12:57 +1100

    Update fucucoin-mn.sh

--- fucucoin-mn.sh
+++ fucucoin-mn.sh
@@ -15,8 +15,8 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 #TCP port
-PORT=20008
-RPC=28888
+PORT=20018
+RPC=22222
 
 #Clear keyboard input buffer
 function clear_stdin { while read -r -t 0; do read -r; done; }
